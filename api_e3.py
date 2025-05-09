be8128b97f151ac645193b1a9f065635b1d2a4d6
Alberto-Lucio <a.jessierlucio@gmail.com>
2025-05-08 22:00:31 -0600

    Update api_e3.py

--- api_e3.py
+++ api_e3.py
@@ -1,4 +1,19 @@
 import json
 import re 
 
-def leer_datos (resultados.json)
+## Rescatar los datos de un archivo JSON
+def leer_datos(json_path= 'resultados.json'):
+    try:
+        with open(json_path, "r", encoding="utf-8") as archivo:
+            datos = json.load(archivo)
+            return datos
+            
+    except FileNotFoundError:
+        print(f"El archivo {json_path} no se encontró.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error al decodificar el archivo {json_path}.")
+        return None
+
+d= leer_datos()
+print(d)
